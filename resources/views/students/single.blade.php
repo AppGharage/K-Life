277c4c66a7dd9a89a2e49bec4bd204dec6680671
tjomega03 <37372913+tjomega03@users.noreply.github.com>
2018-05-28 11:11:10 +0000

    Disabled input for user data

--- resources/views/students/single.blade.php
+++ resources/views/students/single.blade.php
@@ -11,12 +11,28 @@
                         <a onclick="history.go(-1)"class="btn btn-light">Back</a>
                         <br>
                         <div class="container">
-                            <h1>{{$student->firstname}} {{$student->lastname}}</h1>
-                                <p>{{$student->indexNo}}</p>
-                                <hr>
-                                {{$student->email}}
-                                <hr>
-                                {{$student->phone}}
+                            {!! Form::open() !!}
+                            <div class="form-group">
+                                {{form::label('firstname', 'First Name')}}
+                                {{form::text('firstname', $student->firstname, ['class' => 'form-control', 'placeholder' => 'Please Enter Student Firstname','disabled' =>'disabled'] )}}
+                            </div>
+                            <div class="form-group">
+                                {{form::label('lastname', 'Last Name')}}
+                                {{form::text('lastname', $student->lastname, ['class' => 'form-control', 'placeholder' => 'Please Enter Student Lastname','disabled' =>'disabled'])}}
+                            </div>
+                            <div class="form-group">
+                                {{form::label('indexNo', 'Index No')}}
+                                {{form::text('indexNo', $student->indexNo, ['class' => 'form-control', 'placeholder' => 'Please Enter Student Index Number','disabled' =>'disabled'])}}
+                            </div>
+                            <div class="form-group">
+                                {{form::label('email', 'Email')}}
+                                {{form::email('email', $student->email, ['class' => 'form-control', 'placeholder' => 'Please Enter Student Email Address','disabled' =>'disabled'])}}
+                            </div>  
+                            <div class="form-group">
+                                {{form::label('phone', 'Phone')}}
+                                {{form::number('phone', $student->phone, ['class' => 'form-control', 'placeholder' => 'Please Enter Student Phone Number','disabled' =>'disabled'])}}
+                            </div>      
+                        {!! Form::close() !!}
                                 <hr>
                                 <small>Student Created at {{$student->created_at}}</small>
                                 <hr>
