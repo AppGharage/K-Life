2787661fe3d51dbe032f48a84d0b9d72ebe6c8de
tjomega03 <37372913+tjomega03@users.noreply.github.com>
2018-06-08 12:05:28 +0000

    DB Seeders added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
     }
 }
